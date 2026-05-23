3a02f8af6cfa6389405a85f31b4f7b2c493e5106
Nowshinfarhana-Shaik <nowshinfarhanask@gmail.com>
2026-05-23 15:30:36 +0530

    Second step of exceptio handling and logging done

---
+++ src/logger.py
@@ -0,0 +1,25 @@
+import logging
+from exception import CustomException
+import os
+import sys
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
+LOG_FILE_PATH=os.path.join(logs_dir,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format='[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+    )
+
+if __name__=="__main__":
+    logging.info("The logger.py file started ")
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.exception("An error occurred during execution")
+        raise CustomException(e,sys)
