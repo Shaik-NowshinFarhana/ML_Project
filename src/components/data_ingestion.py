b5e0934e3bd07f4ac99f914087c98f88fdc0eab4
Nowshinfarhana-Shaik <nowshinfarhanask@gmail.com>
2026-05-25 10:35:15 +0530

    Hyper parameter tuning done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -26,20 +26,20 @@ class DataIngestion:
         logging.info("Entered the data ingestion method or component")
         try:
             df=pd.read_csv('C:\\Desktop\\ML_Project\\notebook\\data\\stud.csv')
-            logging.info('Read the dataset as dataframe')
+            logging.info('Read the dataset as datafram')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
-            logging.info("Train test split initiated")
+            logging.info("Train test and split initiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Inmgestion of the data iss completed")
+            logging.info("Ingestion of the data is completed")
 
             return(
                 self.ingestion_config.train_data_path,
